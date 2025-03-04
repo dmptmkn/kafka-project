c0356bac434e6e92915b96447a385f3a5b64d8ad
dmptmkn <dm.ptmkn@yandex.ru>
2025-03-04 15:50:41 +0300

    Change configuration for Kraft mode

--- kafka-consumer/src/main/java/org/example/kafkaconsumer/configuration/KafkaConfig.java
+++ kafka-consumer/src/main/java/org/example/kafkaconsumer/configuration/KafkaConfig.java
@@ -13,7 +13,7 @@ public class KafkaConfig {
     @Bean
     public Properties kafkaConsumerProperties() {
         Properties consumerProperties = new Properties();
-        consumerProperties.put("bootstrap.servers", "localhost:9094");
+        consumerProperties.put("bootstrap.servers", "localhost:9097");
         consumerProperties.put("key.deserializer", "org.apache.kafka.common.serialization.LongDeserializer");
         consumerProperties.put("value.deserializer", "io.confluent.kafka.serializers.KafkaAvroDeserializer");
         consumerProperties.put("group.id", "kafka_demo_consumer");
--- kafka-producer/src/main/java/org/example/kafkaproducer/configuration/KafkaConfig.java
+++ kafka-producer/src/main/java/org/example/kafkaproducer/configuration/KafkaConfig.java
@@ -1,7 +1,9 @@
 package org.example.kafkaproducer.configuration;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.example.kafkacore.Alert;
+import org.example.kafkaproducer.partitioner.AlertLevelPartitioner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,9 +15,17 @@ public class KafkaConfig {
     @Bean
     public Properties kafkaProducerProperties() {
         Properties producerProperties = new Properties();
-        producerProperties.put("bootstrap.servers", "localhost:9092,localhost:9093,localhost:9094");
-        producerProperties.put("key.serializer", "org.apache.kafka.common.serialization.LongSerializer");
-        producerProperties.put("value.serializer", "io.confluent.kafka.serializers.KafkaAvroSerializer");
+        producerProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                "localhost:9092,localhost:9096,localhost:9097");
+        producerProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+                "org.apache.kafka.common.serialization.LongSerializer");
+        producerProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                "io.confluent.kafka.serializers.KafkaAvroSerializer");
+        producerProperties.put(ProducerConfig.PARTITIONER_CLASS_CONFIG,
+                AlertLevelPartitioner.class.getName());
+        producerProperties.put(ProducerConfig.ACKS_CONFIG, "all");
+        producerProperties.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "1");
+        producerProperties.put(ProducerConfig.RETRIES_CONFIG, "3");
         producerProperties.put("schema.registry.url", "http://localhost:8081");
 
         return producerProperties;
